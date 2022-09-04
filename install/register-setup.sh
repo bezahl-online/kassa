470fcc35792a0863e6829a3aa9a961cf864c9eb5
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2022-09-04 15:22:25 +0200

    cleanup install

--- install/register-setup.sh
+++ install/register-setup.sh
@@ -3,3 +3,4 @@
 wget https://raw.githubusercontent.com/bezahl-online/kassa/main/bezahlonlinekassa_stable.deb
 sudo apt install -y nginx
 sudo dpkg -i bezahlonlinekassa_stable.deb
+rm bezahlonlinekassa_stable.deb
