e84f28a1405f5e0f0ce244750d2dee545d922a0b
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2022-09-04 13:25:56 +0200

    .

--- install/register-setup.sh
+++ install/register-setup.sh
@@ -2,6 +2,5 @@
 # register setup script
 VERSION="1.2.0"
 wget https://raw.githubusercontent.com/bezahl-online/kassa/main/install/register-setup.sh
--o bezahlonlinekassa.deb https://raw.githubusercontent.com/bezahl-online/kassa/bezahlonlinekassa_stable.deb
 sudo apt install nginx
 sudo dpkg -i bezahlonlinekassa_stable.deb
