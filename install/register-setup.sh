6cda8b74fb787f93dee93f99f91998fcef3f39d8
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2022-09-05 12:08:04 +0200

    refactor installation

--- install/register-setup.sh
+++ install/register-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # register setup script
 wget https://raw.githubusercontent.com/bezahl-online/kassa/main/bezahlonlinekassa_stable.deb
-sudo apt install -y nginx
+sudo apt install -y --no-install-recommends nginx xserver-xorg x11-xserver-utils xinit xinput openbox libnss3-tools
 sudo dpkg -i bezahlonlinekassa_stable.deb
 rm bezahlonlinekassa_stable.deb
