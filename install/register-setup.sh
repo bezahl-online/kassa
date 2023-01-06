e76e55d731fda50bc1c3895d13aa6ae9d7e0acb6
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2023-01-06 21:35:54 +0100

    find pt

--- install/register-setup.sh
+++ install/register-setup.sh
@@ -5,11 +5,14 @@ echo "donwloading register package from Github.."
 wget https://raw.githubusercontent.com/bezahl-online/kassa/main/bezahlonlinekassa_stable.deb
 
 echo "installing X server and Nginx.."
-sudo apt install -y --no-install-recommends nginx xserver-xorg x11-xserver-utils xinit xinput openbox libnss3-tools cups cups-client
+sudo apt install -y --no-install-recommends nmap nginx xserver-xorg x11-xserver-utils xinit xinput openbox libnss3-tools cups cups-client
 
 # chromium browser needed until using tauri version of register
 echo "installing chromium browser.."
 sudo snap install chromium
+
+echo "finding payment terminal.."
+echo $(nmap -p 20007 $(echo $(ip a | grep -Eo 'inet (addr:)?([0-9]*\.){3}' | grep -Eo '([0-9]*\.){3}' | grep -v '127.0.0.')0/24) | tr '\n' ' ' | tr 'N' '\n' | grep open | awk '{print $6}' | tr '()' ' ') pt >>/etc/hosts
 
 echo "installing register.."
 sudo dpkg -i bezahlonlinekassa_stable.deb
